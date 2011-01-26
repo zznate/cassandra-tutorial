39e91df385cf46bd3df55d38f873806a8eb533e6
zznate <nate@vervewireless.com>
2011-01-26 11:50:33 -0600

    project creation and setup

---
+++ src/main/java/com/datastax/tutorial/App.java
@@ -0,0 +1,13 @@
+package com.datastax.tutorial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
